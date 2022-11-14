c9fa1ede569a227df15de4d9c628ddaac39de35b
Hack-Light <ubuntu@ip-172-31-26-121.ec2.internal>
2022-11-14 12:29:06 +0000

    fix

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', '172.31.86.186'),
+            'host'      => env('DB_HOST', '127.0.0.1'),
             'port'      => env('DB_PORT', 3306),
-            'database'  => env('DB_DATABASE', 'homestead'),
-            'username'  => env('DB_USERNAME', 'homestead'),
-            'password'  => env('DB_PASSWORD', 'sePret^i'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
